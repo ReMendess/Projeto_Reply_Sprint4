5e50307bcf90546223e578798083408e2cb135e0
Arthur Rosado <aarthurrosado@gmail.com>
2025-10-01 20:24:55 -0300

    Update 2_Controles.py

--- monitor-factory/pages/2_Controles.py
+++ monitor-factory/pages/2_Controles.py
@@ -24,7 +24,6 @@ dados['Tipo_Encoded'] = encoder.fit_transform(dados[['Tipo']])
 # Carregar modelo treinado
 modelo_path = os.path.join(os.path.dirname(__file__), "..", "model", "modelo_gb.pkl")
 modelo_path = os.path.abspath(modelo_path)
-
 # Carregar modelo
 modelo = joblib.load(modelo_path)
 
