95a7314742a786b67192a7148235b9c01e6d5f45
Renan Mendes <142457377+ReMendess@users.noreply.github.com>
2025-09-30 21:48:18 -0300

    Update 2_Controles.py

--- monitor-factory/pages/2_Controles.py
+++ monitor-factory/pages/2_Controles.py
@@ -8,25 +8,53 @@ from streamlit_autorefresh import st_autorefresh
 import os
 
 st.title("Controle das Máquinas")
-INTERVALO_MS = 30_000  # 30 segundos
-# Verifica se os dados simulados estão disponíveis na sessão
-if "dados_simulados" not in st.session_state:
-    st.error("Os dados simulados ainda não foram gerados. Acesse a página 'Monitoramento da Fábrica' primeiro.")
-    st.stop()
 
+# Função para gerar dados simulados
+def criar_dados_tratados(n_samples=800, seed=42):
+    np.random.seed(seed)
+    id_unico = np.arange(1, n_samples + 1)
+    id_produto = ["M00001"] * n_samples
+    tipo = ["Alta"] * n_samples
 
-# Carrega modelo 
-modelo_path = os.path.join(os.path.dirname(__file__), "..", "model", "modelo_gb.pkl") 
-modelo_path = os.path.abspath(modelo_path) 
+    temp_ar = np.random.normal(loc=298, scale=1, size=n_samples)
+    temp_proc = np.random.normal(loc=308, scale=2, size=n_samples)
+    rotacao = np.random.normal(loc=1500, scale=100, size=n_samples)
+    torque = np.random.normal(loc=40, scale=5, size=n_samples)
+    desgaste = np.clip(np.random.randint(0, 240, size=n_samples), 0, 240)
+
+    falhou = np.random.choice([0, 1], size=n_samples, p=[0.9, 0.1])
+    tipos_falha = ["No Failure", "Power Failure", "Tool Wear Failure", "Overstrain Failure", "Random Failure"]
+    tipo_falha = [np.random.choice(tipos_falha) if f == 1 else "No Failure" for f in falhou]
+
+    df = pd.DataFrame({
+        "ID Unico": id_unico,
+        "ID Produto": id_produto,
+        "Tipo": tipo,
+        "Temperatura do ar [K]": temp_ar,
+        "Temperatura do processo [K]": temp_proc,
+        "Velocidade de rotação [rpm]": rotacao,
+        "Torque [Nm]": torque,
+        "Desgaste ferramenta [min]": desgaste,
+        "Falhou": falhou,
+        "Tipo de falha": tipo_falha
+    })
+    return df
+
+#intervalo
+INTERVALO_MS = 5_000
+
+if "idx" not in st.session_state:
+    st.session_state.idx = 0
+if "hist" not in st.session_state:
+    st.session_state.hist = []
+
+# Carrega modelo
+modelo_path = os.path.join(os.path.dirname(__file__), "..", "model", "modelo_gb.pkl")
+modelo_path = os.path.abspath(modelo_path)
 modelo = joblib.load(modelo_path)
 
-# Usa os dados da sessão
-dados_raw = st.session_state.dados_simulados.copy()
-
-# Mapeia Tipo e cria Tipo_Encoded
-mapa_tipo = {"Baixa": "L", "Média": "M", "Alta": "H"}
-dados = dados_raw.copy()
-dados["Tipo"] = dados["Tipo"].map(mapa_tipo)
+# Gera dados simulados
+dados_raw = criar_dados_tratados(n_samples=800, seed=42)
 
 # Mapeia Tipo e cria Tipo_Encoded
 mapa_tipo = {"Baixa": "L", "Média": "M", "Alta": "H"}
